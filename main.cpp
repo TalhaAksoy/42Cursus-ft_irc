ce5cdc87dfa50a375cefcad2b98c994f38faf971
CheaterAK <ahmetpos3@gmail.com>
2023-02-20 14:47:42 +0300

    update

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 #include <netinet/in.h>
 #include <iostream>
 
-int main()
+int parseBuffer(std::string buffer)
 {
+
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		return (1);
+	int	port = atoi(argv[1]);
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_socket == -1)
 	{
@@ -14,11 +22,10 @@ int main()
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = INADDR_ANY;
-	server_address.sin_port = htons(3131);
+	server_address.sin_port = htons(port);
 
 	if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
 	{
-		// handle error
 		std::cout << "Bind Error" << std::endl;
 		return 1;
 	}
@@ -47,8 +54,8 @@ int main()
 		}
 
 		buffer[bytes_received] = '\0'; // make sure the buffer is null-terminated
-		std::string sendd = ":localhost 001 asd :Welcome to Internet Relay Chat \r\n"; // num erik lere bak 
-		send(client_socket, sendd.c_str(), sizeof(sendd), 0);
+		//std::string sendd = ":localhost 001 asd :Welcome to Internet Relay Chat \r\n"; // num erik lere bak
+		//send(client_socket, sendd.c_str(), sizeof(sendd), 0);
 		printf("Received %ld bytes: %s\n", bytes_received, buffer);
 	}
 	// rest of your code
